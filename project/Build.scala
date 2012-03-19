9401ac3459454754ecda226be513a1c7bb8ce0f3
mumoshu <ykuoka@gmail.com>
2012-03-20 01:00:47 +0900

    version 0.1-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play2-memcached"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "0.1-SNAPSHOT"
 
 
     val appDependencies = Seq(
