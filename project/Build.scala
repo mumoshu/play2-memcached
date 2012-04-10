4c690152cf4582034cffff526204ab32e6dcf1bf
mumoshu <ykuoka@gmail.com>
2012-04-10 23:09:58 +0900

    Confirmed that I could publish jars to Sonatype with the settings.

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,36 @@ object ApplicationBuild extends Build {
     val main = PlayProject(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
       resolvers += "Spy Repository" at "http://files.couchbase.com/maven2",
-      organization := "com.github.mumoshu"
+      organization := "com.github.mumoshu",
+      publishTo <<= version { v: String =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+        else                             Some("releases" at nexus + "service/local/staging/deploy/maven2")
+      },
+      publishMavenStyle := true,
+      publishArtifact in Test := false,
+      pomIncludeRepository := { _ => false },
+      pomExtra := (
+        <url>https://github.com/mumoshu/play2-memcached</url>
+          <licenses>
+            <license>
+              <name>BSD-style</name>
+              <url>http://www.opensource.org/licenses/bsd-license.php</url>
+              <distribution>repo</distribution>
+            </license>
+          </licenses>
+          <scm>
+            <url>git@github.com:mumoshu/play2-memcached.git</url>
+            <connection>scm:git:git@github.com:mumoshu/play2-memcached.git</connection>
+          </scm>
+          <developers>
+            <developer>
+              <id>you</id>
+              <name>KUOKA Yusuke</name>
+              <url>https://github.com/mumoshu</url>
+            </developer>
+          </developers>
+        )
     )
 
     val scalaSample = PlayProject(appName + "-scala-sample", appVersion, path = file("samples/scala"), mainLang = SCALA).settings(
