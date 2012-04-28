6bc747bcdf0463efbaa52986648ed0a3c1a9b4ff
KUOKA Yusuke <ykuoka@gmail.com>
2012-04-28 17:36:47 +0900

    Bump to 0.2-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play2-memcached"
-    val appVersion      = "0.1-SNAPSHOT"
+    val appVersion      = "0.2-SNAPSHOT"
 
   lazy val root = Project("root", base = file("."))
     .dependsOn(plugin)
