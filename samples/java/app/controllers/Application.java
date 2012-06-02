954d6d93a2f5507813596ff001f4786002aba805
mumoshu <ykuoka@gmail.com>
2012-06-02 14:51:43 +0900

    Fix java spec

--- samples/java/app/controllers/Application.java
+++ samples/java/app/controllers/Application.java
@@ -7,50 +7,52 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
+
+    private static String key = "theKeyForJavaSample";
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
 
     public static Result set() {
-        Cache.set("key", "cached value");
+        Cache.set(key, "cached value");
         return ok("Cached.");
     }
 
     public static Result get() {
-        return ok("Cached value = " + Cache.get("key"));
+        return ok("Cached value = " + Cache.get(key));
     }
 
     public static Result delete() {
-        Cache.set("key", "foooo", 0);
+        Cache.set(key, "foooo", 0);
         return ok("Deleted.");
     }
 
     public static Result cacheInt() {
-        Cache.set("key", new Integer(123));
+        Cache.set(key, new Integer(123));
 
-        Object cachedValue = Cache.get("key");
+        Object cachedValue = Cache.get(key);
 
         if (cachedValue != null) {
             Integer integerValue = (Integer)cachedValue;
-            return ok(cachedValue.toString() + cachedValue.getClass() + " " + integerValue + " " + integerValue.getClass());
-            // 123 class java.lang.Integer 123 int
+            return ok(integerValue + " " + integerValue.getClass().getName());
+            // 123 java.lang.Integer
         } else {
             throw new RuntimeException("Could not get the cached value.");
         }
     }
 
     public static Result cacheString() {
-        Cache.set("key", "mikoto");
+        Cache.set(key, "mikoto");
 
-        return ok(Cache.get("key").toString());
+        return ok(Cache.get(key).toString());
         // Some(mikoto)
     }
 
     public static Result cacheBool() {
-        Cache.set("key", new Boolean(true));
+        Cache.set(key, new Boolean(true));
 
-        Object cachedValue = Cache.get("key");
+        Object cachedValue = Cache.get(key);
 
         if (cachedValue != null) {
             Boolean booleanValue = (Boolean)cachedValue;
--- samples/java/test/JavaSampleTest.java
+++ samples/java/test/JavaSampleTest.java
@@ -6,6 +6,7 @@ import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static play.test.Helpers.*;
@@ -15,12 +16,13 @@ public class JavaSampleTest {
 
     @Test
     public void cachingIntValue() {
-        Map<String, String> additionalConfiguration = inMemoryDatabase();
+        Map<String, String> additionalConfiguration = new HashMap<String, String>();
         additionalConfiguration.put("ehcacheplugin", "disabled");
+        additionalConfiguration.put("memcached.host", "127.0.0.1:11211");
         running(fakeApplication(additionalConfiguration), new Runnable() {
             public void run() {
-                Result result = routeAndCall(fakeRequest(GET, "/cache/set"));
-                assertThat(contentAsString(result)).isEqualTo("123: java.lang.Integer");
+                Result result = routeAndCall(fakeRequest(GET, "/cacheInt"));
+                assertThat(contentAsString(result)).isEqualTo("123 java.lang.Integer");
             }
         });
     }
