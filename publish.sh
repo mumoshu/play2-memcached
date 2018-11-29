bc614a45f21295bf6c997c2f72b21aae7068f332
Matthias Kurz <m.kurz@irregular.at>
2018-11-29 16:05:18 +0100

    Play 2.7.0-RC8

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PLAY_VERSION=2.7.0-RC3 sbt ++2.11.12 publishSigned
-PLAY_VERSION=2.7.0-RC3 sbt ++2.12.7 publishSigned
+PLAY_VERSION=2.7.0-RC8 sbt ++2.11.12 publishSigned
+PLAY_VERSION=2.7.0-RC8 sbt ++2.12.7 publishSigned
 # not working yet, wait for final Play 2.7 and Scala 2.13 release:
-#PLAY_VERSION=2.7.0-RC3 sbt ++2.13.0-M4 publishSigned
+#PLAY_VERSION=2.7.0-RC8 sbt ++2.13.0-M4 publishSigned
