4264dd5e374207a762837b68d82222425ebea73d
Matthias Kurz <m.kurz@irregular.at>
2023-11-24 11:37:38 +0100

    Fix tests to make them work with Scala 3

--- samples/scala/test/ApplicationSpec.scala
+++ samples/scala/test/ApplicationSpec.scala
@@ -29,15 +29,19 @@ class ApplicationSpec extends Specification {
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication(app){
-      route(this.app, FakeRequest(GET, "/boum")) must beSome[Future[Result]].which (status(_) == NOT_FOUND)
+      override def running() = {
+        route(this.app, FakeRequest(GET, "/boum")) must beSome[Future[Result]].which (status(_) == NOT_FOUND)
+      }
     }
 
     "render the index page" in new WithApplication(app){
-      val home = route(this.app, FakeRequest(GET, "/")).get
+      override def running() = {
+        val home = route(this.app, FakeRequest(GET, "/")).get
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome[String].which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+        status(home) must equalTo(OK)
+        contentType(home) must beSome[String].which(_ == "text/html")
+        contentAsString(home) must contain ("Your new application is ready.")
+      }
     }
   }
 
@@ -56,22 +60,26 @@ class ApplicationSpec extends Specification {
   "The scala sample application" should {
 
     "return a cached data" in new WithApplication(app){
-      connectingLocalMemcached(this.app) {
-        c(this.app, "/cache/get") must equalTo ("None")
-        c(this.app, "/cache/set") must equalTo ("Cached.")
-        c(this.app, "/cache/get") must equalTo ("Some(cached value)")
-        c(this.app, "/session/get") must equalTo ("None")
-        c(this.app, "/session/set") must equalTo ("Cached.")
-        c(this.app, "/session/get") must equalTo ("Some(session value)")
-        c(this.app, "/cache/get") must equalTo ("Some(cached value)")
+      override def running() = {
+        connectingLocalMemcached(this.app) {
+          c(this.app, "/cache/get") must equalTo ("None")
+          c(this.app, "/cache/set") must equalTo ("Cached.")
+          c(this.app, "/cache/get") must equalTo ("Some(cached value)")
+          c(this.app, "/session/get") must equalTo ("None")
+          c(this.app, "/session/set") must equalTo ("Cached.")
+          c(this.app, "/session/get") must equalTo ("Some(session value)")
+          c(this.app, "/cache/get") must equalTo ("Some(cached value)")
+        }
       }
     }
 
     "return expected results" in new WithApplication(app){
-      connectingLocalMemcached(this.app) {
-        c(this.app, "/cacheInt") must equalTo ("123class java.lang.Integer 123 int")
-        c(this.app, "/cacheString") must equalTo ("Some(mikoto)")
-        c(this.app, "/cacheBool") must equalTo ("true : class java.lang.Boolean, true : boolean")
+      override def running() = {
+        connectingLocalMemcached(this.app) {
+          c(this.app, "/cacheInt") must equalTo ("123class java.lang.Integer 123 int")
+          c(this.app, "/cacheString") must equalTo ("Some(mikoto)")
+          c(this.app, "/cacheBool") must equalTo ("true : class java.lang.Boolean, true : boolean")
+        }
       }
     }
   }
